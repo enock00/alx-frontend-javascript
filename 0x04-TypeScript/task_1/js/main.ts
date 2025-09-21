333726a2615dfede8ac12cba692cb6f1c640590e
Enock <enockzy54@gmail.com>
2025-09-21 23:42:34 +0300

    writting StudentClass class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -41,3 +41,35 @@ function printTeacher({ firstName, lastName }: { firstName: string; lastName: st
 }
 
 console.log(printTeacher({ firstName: "John", lastName: "Doe" }));
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+class StudentClass implements StudentClassInterface {
+  private firstName: string;
+  private lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+
+const student: StudentClassInterface = new StudentClass("John", "Doe");
+console.log(student.displayName());
+console.log(student.workOnHomework());
